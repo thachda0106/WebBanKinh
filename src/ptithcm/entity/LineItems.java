2179cc5415d661642437cb0b238e02ed36bde44e
unknown <nguyentrong1606@gmail.com>
2021-11-14 12:44:57 +0700

    add entity

--- src/ptithcm/entity/LineItems.java
+++ src/ptithcm/entity/LineItems.java
@@ -1,5 +1,52 @@
 package ptithcm.entity;
+import java.io.Serializable;
 
-public class LineItems {
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "lineitems")
+public class LineItems implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@ManyToOne
+	@JoinColumn(name="product_id", referencedColumnName = "id")
+	private Product product;
+	
+	@Id
+	@ManyToOne
+	@JoinColumn(name="orders_id", referencedColumnName = "id")
+	private Orders orders;
+	
+	private int quantity;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Orders getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Orders orders) {
+		this.orders = orders;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 
 }
--- src/ptithcm/entity/Orders.java
+++ src/ptithcm/entity/Orders.java
@@ -1,5 +1,67 @@
 package ptithcm.entity;
 
-public class Orders {
+import java.util.Collection;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="orders")
+public class Orders {
+	@Id
+	@GeneratedValue
+	private int id;
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date datetime;
+	private boolean status;
+	
+	@ManyToOne
+	@JoinColumn(name="customer_id")
+	private Customer customer;
+	
+	@OneToMany(mappedBy = "orders", fetch =FetchType.EAGER)
+	private Collection<LineItems> lineitems;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(Date datetime) {
+		this.datetime = datetime;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Collection<LineItems> getLineitems() {
+		return lineitems;
+	}
+	public void setLineitems(Collection<LineItems> lineitems) {
+		this.lineitems = lineitems;
+	}
 }
